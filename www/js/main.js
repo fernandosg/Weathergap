599f03bdadb5d5823fae16e85458d5424cbcb39a
fernandosg <fsg_tmp@hotmail.com>
2017-07-12 19:08:53 -0500

    Checking if the setting are changed to geoposition, then the information of the weather is requested by the city name getting form google places api, if not, is getting from the city name saved before.

--- www/js/main.js
+++ www/js/main.js
@@ -17,19 +17,33 @@ var app = phonon.navigator();
  * page events such as onCreate, onReady, etc
 */
 app.on({page: 'home', preventClose: false, content: null},function(activity){
+  var city_for_weather="Tampico";
   var onAction = function(evt) {
 
   };
   var creating=false;
+  var changeTitleWeatherToday=function(setting_for_detection){
+    document.getElementById("title_weather_today").innerHTML="Actual weather in "+city_for_weather+", detecting from "+setting_for_detection;
+  }
+
   activity.onCreate(function() {
-    getInformationApi();
+    if(localStorage.getItem("city_for_weather")==undefined){
+      localStorage.setItem("city_for_weather",city_for_weather);
+    }
+    city_for_weather=localStorage.getItem("city_for_weather");
+    if(localStorage.getItem("detection")=="geoposition")
+      getPosition();
+    else{
+      getInformationApi();
+      changeTitleWeatherToday("city saved or default");
+    }
     creating=true;
   });
 
   function getInformationApi(){
     var url="http://api.apixu.com/v1/current.json?"
     url+="key="+key+"&";
-    url+="q=Panuco";
+    url+="q="+city_for_weather;
     xmlHTTP=new XMLHttpRequest();
     xmlHTTP.onreadystatechange=processWeather;
     xmlHTTP.open("GET",url,true);
@@ -60,11 +74,43 @@ app.on({page: 'home', preventClose: false, content: null},function(activity){
   */
   activity.onReady(function(){
     if(creating===false && localStorage.getItem("detection")!=state_before){
-      getInformationApi();
+      if(localStorage.getItem("detection")=="geoposition"){
+        getPosition();
+      }else {
+        getInformationApi();
+        changeTitleWeatherToday("city saved or default")
+      }
     }else
       creating=false;
     state_before=localStorage.getItem("detection");
   })
+
+  var getPosition=function(){
+    var options={enableHighAccuracy:true,timeout:50000};
+    navigator.geolocation.getCurrentPosition(function(position){
+      var lat,lng;
+      lat=position.coords.latitude;
+      lng=position.coords.longitude;
+      getCityName(lat,lng);
+    })
+  }
+
+  var getCityName=function(lat,lng){
+    var url="http://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+"&sensor=true";
+    var xmlHTTP=new XMLHttpRequest();
+    xmlHTTP.onreadystatechange=function(){
+      if(xmlHTTP.readyState==4){
+        var jsonResult=xmlHTTP.responseText
+        var result=JSON.parse(jsonResult);
+        city_for_weather=result.results[2].address_components[0].long_name;
+        localStorage.setItem("city_for_weather",city_for_weather);
+        changeTitleWeatherToday("geoposition");
+        getInformationApi();
+      }
+    };
+    xmlHTTP.open("GET",url,true);
+    xmlHTTP.send();
+  }
 
   activity.onHashChanged(function(pizza) {
       //document.querySelector('.pizza').textContent = pizza;
